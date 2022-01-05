0396475024c067d2f6ff2da74832d99edb0cdf43
shuty21 <bbshuty.devlife@gmail.com>
2022-01-06 00:16:03 +0900

    スポット検索パラメータ調節

--- d_wheres/pages/tmp.vue
+++ d_wheres/pages/tmp.vue
@@ -2,14 +2,14 @@
   <v-card max-width="100%" class="mx-auto">
     <v-btn><v-icon>mdi-account</v-icon>user</v-btn>
     <v-btn><v-icon>mdi-map-marker</v-icon>spot</v-btn>
-    <v-toolbar
+    <!-- <v-toolbar
       color="cyan"
       dark
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <!-- <v-toolbar-title> -->
-      <!-- </v-toolbar-title> -->
-        <v-text-field
+       <v-toolbar-title>
+       </v-toolbar-title>
+         <v-text-field
           label="Search Spot Name"
           v-model="searchParams.spot.name"
         ></v-text-field>
@@ -22,9 +22,9 @@
       <v-btn icon>
         <v-icon>mdi-view-module</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-toolbar> -->
 
-    <v-container fluid>
+    <!-- <v-container fluid>
       <v-row align="center">
         <v-col cols="12">
           <v-autocomplete
@@ -71,8 +71,171 @@
           ></v-autocomplete>
         </v-col>
       </v-row>
-    </v-container>
-
+    </v-container> -->
+    <template v-slot:progress>
+      <v-progress-linear
+        absolute
+        color="green lighten-3"
+        height="4"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+    <v-img
+      height="200"
+      src="https://cdn.vuetifyjs.com/images/cards/dark-beach.jpg"
+    >
+      <v-row>
+        <v-col
+          class="text-right"
+          cols="12"
+        >
+          <v-menu
+            bottom
+            left
+            transition="slide-y-transition"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="isUpdating = true">
+                <v-list-item-action>
+                  <v-icon>mdi-cog</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>Update</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-col>
+        <v-row
+          class="pa-4"
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <h3 class="text-h5">
+              {{ name }}
+            </h3>
+            <span class="grey--text text--lighten-1">{{ title }}</span>
+          </v-col>
+        </v-row>
+      </v-row>
+    </v-img>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-text-field
+              v-model="name"
+              :disabled="isUpdating"
+              filled
+              color="blue-grey lighten-2"
+              label="Name"
+            ></v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-text-field
+              v-model="title"
+              :disabled="isUpdating"
+              filled
+              color="blue-grey lighten-2"
+              label="Title"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-autocomplete
+              v-model="friends"
+              :disabled="isUpdating"
+              :items="master.spot.prefectures"
+              filled
+              chips
+              color="blue-grey lighten-2"
+              label="Select"
+              item-text="name"
+              item-value="name"
+              multiple
+            >
+            <v-autocomplete
+              v-model="values"
+              :items="master.spot.prefectures"
+              outlined
+              dense
+              chips
+              small-chips
+              label="Prefectures"
+              multiple
+            ></v-autocomplete>
+              <template v-slot:selection="data">
+                <v-chip
+                  v-bind="data.attrs"
+                  :input-value="data.selected"
+                  close
+                  @click="data.select"
+                  @click:close="remove(data.item)"
+                >
+                  <v-avatar left>
+                    <v-img :src="data.item.avatar"></v-img>
+                  </v-avatar>
+                  {{ data.item.name }}
+                </v-chip>
+              </template>
+              <template v-slot:item="data">
+                <template v-if="typeof data.item !== 'object'">
+                  <v-list-item-content v-text="data.item"></v-list-item-content>
+                </template>
+                <template v-else>
+                  <v-list-item-avatar>
+                    <img :src="data.item.avatar">
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title v-html="data.item.name"></v-list-item-title>
+                    <v-list-item-subtitle v-html="data.item.group"></v-list-item-subtitle>
+                  </v-list-item-content>
+                </template>
+              </template>
+            </v-autocomplete>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-switch
+        v-model="autoUpdate"
+        :disabled="isUpdating"
+        class="mt-0"
+        color="green lighten-2"
+        hide-details
+        label="Auto Update"
+      ></v-switch>
+      <v-spacer></v-spacer>
+      <v-btn
+        :disabled="autoUpdate"
+        :loading="isUpdating"
+        color="blue-grey darken-3"
+        depressed
+        @click="isUpdating = true"
+      >
+        <v-icon left>
+          mdi-magnify
+        </v-icon>
+        Search
+      </v-btn>
+    </v-card-actions>
     <v-list three-line>
       <template v-for="(item, index) in items">
         <v-subheader
