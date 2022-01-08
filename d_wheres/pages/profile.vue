885a009ba7575be548143aa840937ec4cab6f2e2
shuty21 <bbshuty.devlife@gmail.com>
2022-01-09 00:09:02 +0900

    Profile デフォルト画像 assetsに配置

--- d_wheres/pages/profile.vue
+++ d_wheres/pages/profile.vue
@@ -1,10 +1,10 @@
 <template>
   <v-card class="mx-auto" max-width="1000" tile>
-    <v-img height="200" :src="background"></v-img>
+    <v-img class="u_b_img" height="200" :src="background"></v-img>
     <v-row style="margin:2.5%;position:absolute; top: 130px">
       <v-list-item>
         <v-list-item-avatar size="100">
-          <v-img class="p_img" :src="image" alt="John"></v-img>
+          <v-img class="u_img" :src="image" alt="John"></v-img>
         </v-list-item-avatar>
         <v-list-item-content>
           <v-list-item-title class="title" style="margin-top:20px;">{{ user_name }}</v-list-item-title>
@@ -22,8 +22,8 @@
         </div>
       </v-list-item>
       <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>Single-line item</v-list-item-title>
+        <v-list-item-content class="bio">
+          {{ biography }}
         </v-list-item-content>
       </v-list-item>
 
@@ -57,10 +57,11 @@ export default {
     return {
       user_name: null,
       user_code: null,
-      image: null,
+      image: require("@/assets/user.svg"),
       background: null,
       follows: 0,
       followers: 0,
+      biography: '',
       genres: [],
       is_self: false,
       setting: {
@@ -89,6 +90,7 @@ export default {
           this.user_code = userData.code
           this.image = userData.profile.image
           this.background = userData.profile.background
+          this.biography = userData.profile.biography
           this.follows = userData.profile.follows
           this.followers = userData.profile.followers
           this.genres = userData.profile.genres
@@ -122,13 +124,26 @@ export default {
         .catch((err) => {
           console.log(err);
         });
+    },
+    htmlText(msg){
+      if( !!(msg) ){
+        return msg.replace(/\r?\n/g, '<br>')
+      }
     }
   }
 }
 </script>
 
 <style>
-.p_img {
+.u_b_img {
+  background: linear-gradient( darkgray, #626263);
+}
+.u_img {
   border: 4px black solid;
+  background-color: darkgray;
+}
+.bio {
+  /* 改行コード有効化 */
+  white-space: pre-wrap;
 }
 </style>
